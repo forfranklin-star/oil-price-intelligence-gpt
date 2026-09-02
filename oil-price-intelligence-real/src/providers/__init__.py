6fd582809065bba749e401f0da0ae9e183fba498
forfranklin-star <forfranklin@gmail.com>
2026-09-02 14:51:50 +0800

    Delete oil-price-intelligence-real directory

--- oil-price-intelligence-real/src/providers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .market import MarketProvider
-from .macro import FredProvider
-from .news import NewsProvider
-from .diesel import ChinaDieselProvider
-
-__all__ = ["MarketProvider", "FredProvider", "NewsProvider", "ChinaDieselProvider"]
